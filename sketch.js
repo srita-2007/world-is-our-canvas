8966ea123000385745eed1ab52130cd08f30414f
srita-2007 <65296519+srita-2007@users.noreply.github.com>
2020-06-21 21:39:43 -0700

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,29 @@
+var ball;
+
+function setup(){
+    createCanvas(600,600);
+    ball = createSprite(300,300,20,20);
+    ball.shapeColor = "lavender";
+}
+
+function draw(){
+    background("black");
+    if(keyDown(LEFT_ARROW)){
+        changePosition(-1,0);
+    }
+    else if(keyDown(RIGHT_ARROW)){
+        changePosition(1,0);
+    }
+    else if(keyDown(UP_ARROW)){
+        changePosition(0,-1);
+    }
+    else if(keyDown(DOWN_ARROW)){
+        changePosition(0,+1);
+    }
+    drawSprites();
+}
+
+function changePosition(x,y){
+    ball.x = ball.x + x;
+    ball.y = ball.y + y;
+}
